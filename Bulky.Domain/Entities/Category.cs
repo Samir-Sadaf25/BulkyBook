4e593f7282152275b407b9565c24196282068eb5
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2026-06-03 15:52:24 +0600

    feat : Create Category • add Category UI • add category to db • dynamic input with tag helper

--- Bulky.Domain/Entities/Category.cs
+++ Bulky.Domain/Entities/Category.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace Bulky.Domain
 {
     public class Category
@@ -7,7 +7,9 @@ namespace Bulky.Domain
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
